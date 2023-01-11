c05fed68177601c3e656bb310ab9be0b0dd840e5
Magicsong111 <120069972+Magicsong111@users.noreply.github.com>
2023-01-11 19:21:25 +0800

    Add files via upload

---
+++ download/DL.js
@@ -0,0 +1,25 @@
+//2023/1/11星期三
+function dl(name)
+{
+	if(name=="WIN3.0")
+	{
+		window.open("https://winworldpc.com/download/c3a1c398-c2b9-c2a6-c3a6-c39211c3a6ef/from/c3ae6ee2-8099-713d-3411-c3a6e280947e");
+	}
+	else if(name=="OFFICE3.0")
+	{
+		window.open("https://winworldpc.com/download/c1665bc5-8d26-11ec-84e0-0200008a0da4/from/c3ae6ee2-8099-713d-3411-c3a6e280947e");
+	}
+	else if(name=="MSDOS6.22")
+	{
+		window.open("https://dqunying2.jb51.net/201804/tools/MS-DOS622_jb51.rar");
+	}
+	else if(name=="MCGCG")
+	{
+		window.open("./Minecraft Gift Code Generator.exe");
+	}
+	else if(name=="QB")
+	{
+		window.open("https://winworldpc.com/download/c385e280-b043-c2b6-c2a6-5211c3a5c28f/from/c3ae6ee2-8099-713d-3411-c3a6e280947e");
+	}
+}
+
